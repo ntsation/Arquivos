13a37cc0bffa204f080709dd36dfe949b87d28b7
ntsation <naathanmarinho11@gmail.com>
2022-12-31 11:34:33 -0300

    html ordenado

--- Source/Scripts/avengers/pub_avengers.py
+++ Source/Scripts/avengers/pub_avengers.py
@@ -6,4 +6,4 @@ df = pd.read_csv(arquivo, encoding='unicode_escape')
 df = df.drop(columns = 'URL').drop(columns = 'Probationary Introl')
 df = df[df.columns[0:7]]
 df_ordenado = df.sort_values(by='Appearances',ascending=False)
-html = df.to_html('avengers.html')
+html = df_ordenado.to_html('avengers.html')
